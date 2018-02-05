669cf0601208d3d355cebba758c677d9fd077b47
ume3 <bo2tani@gmail.com>
2018-02-05 18:17:17 +0900

    dockerへserverspecを流す設定にする
    image名はbuildで取得する｡

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,9 @@
 require 'serverspec'
+require 'docker'
 
-set :backend, :exec
+image = Docker::Image.build_from_dir('./')
+
+set :backend, :docker
+set :os, family: 'RedHat', arch: 'x86_64'
+set :docker_image, image.id
 
