c6ecd6351b85429dba8f243c30e6f56bc70264d5
ume3 <bo2tani@gmail.com>
2018-03-06 19:35:24 +0900

    lxc-attachの動作確認

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,22 +7,3 @@ set :os, family: 'RedHat', arch: 'x86_64'
 if ENV['DOCKER_CONTAINER']
   set :docker_container, ENV['DOCKER_CONTAINER']
 end
-
-# https://circleci.com/docs/docker#docker-exec
-if ENV['CIRCLECI']
-  module Docker
-    class Container
-      def exec(command, opts = {}, &block)
-        command[2] = command[2].inspect # ['/bin/sh', '-c', 'YOUR COMMAND']
-        cmd = %Q{sudo lxc-attach -n #{self.id} -- #{command.join(' ')}}
-        stdin, stdout, stderr, wait_thread = Open3.popen3 cmd
-        [stdout.read, [stderr.read], wait_thread.value.exitstatus]
-      end
-      def remove(options={})
-        # do not delete container
-      end
-      alias_method :delete, :remove
-      alias_method :kill, :remove
-    end
-  end
-end
